dc2bb010a3743a7d1965cb8797baf4f31c026945
Dmitry <demx@demx.ru>
2012-10-07 18:13:31 +0300

    Календарик

--- js/js.js
+++ js/js.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+var date = new Date();
+$('#sidebar').DatePicker({
+	flat: true,
+	date: '2008-07-31',
+	current: date.getFullYear()+'-'+(parseInt(date.getMonth())+1)+'-'+date.getDate(),
+	calendars: 1,
+	starts: 1
+});
 	//небольшой хак, чтобы с localStorage можно было работать как с объектом
 	if(isLocalStorageAvailable()) {
 		if(!localStorage['storage'])
@@ -17,7 +25,6 @@ $(document).ready(function(){
 		wday[4]="Чт"
 		wday[5]="Пт"
 		wday[6]="Сб"
-		var date = new Date();
         var time = wday[date.getDay()]+', '+(date.getHours()<10?'0':'')+date.getHours()+':'+(date.getMinutes()<10?'0':'')+date.getMinutes();
         $("#date").html(time);
         window.setTimeout(arguments.callee, 1000);
@@ -38,7 +45,22 @@ $(document).ready(function(){
 });
 
 //localStorage.removeItem('storage');
+function textNode(nodeText, nodeClass, nodeId) {
+	return "<span class='"+nodeClass+"' id='"+nodeId+"'>"+nodeText+"</span>"; 
+} 
 
+function editNode(editType, editSize, editClass, editId) {
+	switch (editType) {
+		case "text":
+			return "<input type='text' class='"+editClass+"' id='"+editId+"' size='"+editSize[0]+"' />";
+		case "textarea":
+			return "<textarea class='"+editClass+"' id='"+editId+"' cols='"+editSize[0]+" rows='"+editSize[1]+"'></textarea>";
+	}		
+} 
+
+function buttonNode(buttonText, buttonClass, buttonId) {
+	return "<input type='button' value='"+buttonText+"' class='"+buttonClass+"' id='"+buttonId+"' />";
+}
 
 //поддерживает ли клиент localStorage?
 function isLocalStorageAvailable() {
@@ -58,7 +80,34 @@ function writeToStorage() {
 //функция создания базового события в календаре
 function createEvent(name, date)
 {
-	var event = { 'name':name, 'date':date };
+	var id = myStorage['events'].length++;
+	var event = { 'id':id, 'name':name, 'date':date };
 	myStorage['events'].push(event);
 	writeToStorage();
+	return id;
+}
+
+//удаление события
+function deleteEvent(id)
+{
+	delete myStorage['events'][id];
+	writeToStorage();
+}
+
+//получить событие
+//function getEvent(id) {
+	
+
+//отрисовывает нужную форму
+function draw(html) {
+$("#main").append(html);
+}
+
+//форма добавления события
+function drawAddForm() {
+	var html = '';
+	html += textNode("Название события", "tlable", "");
+	html += editNode("text", [15], "tedit", "eventName");
+	html += buttonNode("Okay", "tbutton", "");
+	draw(html);
 }
